a45250e95fe18a6682c7bed5961a30e3defe7ac6
zrony <95293910+zrony@users.noreply.github.com>
2023-07-29 20:36:21 +0300

    final project

--- Functions.py
+++ Functions.py
@@ -16,7 +16,7 @@ def longest_subsequence_nlogn(coordinates):
     length = 1
 
     for i in range(1, n):
-        # Binary search to find the position of current coordinate in ends list
+        # Binary search to find the position of the current coordinate in the ends list
         left, right = 0, length
         while left < right:
             mid = (left + right) // 2
@@ -42,48 +42,37 @@ def longest_subsequence_nlogn(coordinates):
     return length, longest_subseq[::-1]
 
 
-def finding_heaviest_subsequence(coordinates):
-    dp = [0] * len(coordinates)  # Initialize the dp table
-    dp[0] = coordinates[0][2]  # Set the weight of the first point as the first value in the dp table
+def heaviest_increasing_subsequence(coordinates):
+    # Sort the list based on x and then y
+    sorted_coords = sorted(coordinates, key=lambda x: (x[0], x[1]))
 
-    parent = [-1] * len(coordinates)  # To keep track of the path
+    n = len(sorted_coords)
+    dp = [0] * n
+    prev = [-1] * n  # store predecessors
 
-    # Fill up the dp table
-    for i in range(1, len(coordinates)):
+    # Initialize the dp array with weights of tuples
+    for i in range(n):
+        dp[i] = sorted_coords[i][2]
+
+    # Iterate over all tuples to find the maximum weight
+    for i in range(n):
         for j in range(i):
-            # Update dp[i] only if the x and y coordinates of j-th point are strictly less than the x and y
-            # coordinates of i-th point
-            if coordinates[j][1] < coordinates[i][1] and dp[i] < dp[j]:
-                dp[i] = dp[j]
-                parent[i] = j
-        dp[i] += coordinates[i][2]
+            if sorted_coords[j][0] < sorted_coords[i][0] and sorted_coords[j][1] < sorted_coords[i][1]:
+                if dp[j] + sorted_coords[i][2] > dp[i]:
+                    dp[i] = dp[j] + sorted_coords[i][2]
+                    prev[i] = j 
 
     # Find the index with the maximum weight
-    max_index = dp.index(max(dp))
+    max_idx = dp.index(max(dp))
 
-    # Retrieve the path
+    # Traceback the path
     path = []
-    weight_sum = 0  # Initialize the sum of weights
-    while max_index != -1:
-        path.append(coordinates[max_index])
-        weight_sum += coordinates[max_index][2]  # Add the weight of the current point to the sum of weights
-        max_index = parent[max_index]
+    while max_idx != -1:
+        path.append(sorted_coords[max_idx])
+        max_idx = prev[max_idx]
     path.reverse()
 
-    return weight_sum, path
-
-
-def convert_excel_to_array(path, sheet_name):
-    df = pd.read_excel(path, sheet_name=sheet_name)
-    x = df['Column 1'].values
-    y = df['Column 2'].values
-
-    coordinates = []
-
-    for i in range(len(x)):
-        coordinates.append((x[i], y[i], 1))
-
-    return coordinates
+    return max(dp), path
 
 # first algorithm
 def update_weights_randomly(coordinates):
@@ -124,10 +113,14 @@ def update_weights_iterative(coordinates):
     changeable_points = set(possible_points)
 
     # check every point in iterative order, and if it's not part of the heaviest path - change its weight to 2
+    count = 0
     for point in changeable_points:
+        count +=1
         index = coordinates.index(point)
         coordinates[index] = (point[0], point[1], 2)
-        new_W, sub = finding_heaviest_subsequence(coordinates)
+        new_W, sub= heaviest_increasing_subsequence(coordinates)
+        if count % 10 == 0:
+            print(count)
         if new_W <= W:
             sub_array.append(coordinates[index])
         else:
@@ -156,20 +149,22 @@ def update_weights_intelligence_algorithm(coordinates):
     changeablePoints = set(coordinates)
     # Initialize the 'changedPoints' set to keep track of points with updated weights
     changedPoints = set()
-
+    count = 0
     # Iterate until the 'changeablePoints' set is empty
     while changeablePoints:
-
+        count +=1
         # Choose the point with one of the methods
         target_coord = find_coordinate_with_largest_difference(changeablePoints)
         index = coordinates.index(target_coord)
         updated_coord = target_coord[:2] + (2,)  # increase weight
         coordinates[index] = updated_coord  # update coordinate
-        new_weight, chain = finding_heaviest_subsequence(coordinates)
+        new_weight, chain = heaviest_increasing_subsequence(coordinates)
+        if count % 10 == 0:
+            print(count)
 
         # Check if the new weight is heavier than the heaviest chain
         if new_weight > W:
-            # break, return to original state
+            # break, return to the original state
             coordinates[index] = target_coord
         else:
             changedPoints.add(updated_coord)  # We can change the point's weight.
@@ -177,4 +172,18 @@ def update_weights_intelligence_algorithm(coordinates):
         # Remove the checked point from the 'changeablePoints' set
         changeablePoints.remove(target_coord)  # Remove the target coordinate from the set
 
-    return changedPoints
+    return len(changedPoints), changedPoints
+
+# get coordinates from the Excel
+def convert_excel_to_array(path, sheet_name):
+    df = pd.read_excel(path, sheet_name=sheet_name)
+    x = df['Column 1'].values
+    y = df['Column 2'].values
+
+    coordinates = []
+
+    for i in range(len(x)):
+        coordinates.append((x[i], y[i], 1))
+
+    return coordinates
+
